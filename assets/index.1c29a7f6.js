2ba8145087a54f8d5cbb4009912e4b3aa9e13b8b
MrBroky <99945865+MrBroky@users.noreply.github.com>
2022-02-18 01:03:02 -0500

    init

---
+++ assets/index.1c29a7f6.js
@@ -0,0 +1,32 @@
+import{S as l,i as n,s as d,e as p,a as h,b as m,n as i,d as u}from"./vendor.3bae58bb.js";const g=function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const o of s.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&a(o)}).observe(document,{childList:!0,subtree:!0});function r(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerpolicy&&(s.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?s.credentials="include":e.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function a(e){if(e.ep)return;e.ep=!0;const s=r(e);fetch(e.href,s)}};g();function v(c){let t;return{c(){t=p("main"),t.innerHTML=`<div class="navbar bg-base-100"><a href="https://www.youtube.com/mrbroky" class="btn btn-ghost normal-case text-xl">MrBroky</a></div> 
+
+  <div class="hero min-h-screen" style="background-image: url(&quot;./public/brokygalaxy.png&quot;);"><div class="hero-overlay bg-opacity-60"></div> 
+    <div class="text-center hero-content text-neutral-content"><div class="max-w-md"><h1 class="mb-5 text-5xl font-bold">MrBroky</h1> 
+        <p class="mb-5">Just a guy with a dream.</p></div></div></div> 
+
+  <div></div> 
+
+  
+  <div class="flex justify-around mt-6 "><div class="card w-96 bg-base-100 shadow-xl"><figure class="px-10 pt-10"><img src="./brokillo.jpeg" alt="Shoes" class="rounded-xl mask mask-hexagon"/></figure> 
+      <div class="card-body items-center text-center"><h2 class="card-title">Watch me LIVE!</h2> 
+        <p>Rocket League, Minecraft, Phasmophobia, and more.</p> 
+        <div class="card-actions"><button class="btn btn-primary"><a href="https://www.youtube.com/mrbroky">YouTube</a></button></div></div></div> 
+    
+    <div class="card w-96 shadow-xl bg-accent text-accent-content"><figure class="px-10 pt-10"><img src="./logobroky.png" alt="Shoes" class="rounded-xl mask mask-hexagon"/></figure> 
+      <div class="card-body items-center text-center"><h2 class="card-title">Watch more of my videos!</h2> 
+        <p>Videos, Hightlights, Shorts and Vlogs(coming soon)</p> 
+        <div class="card-actions"><button class="btn btn-primary"><a href="https://www.twitch.tv/mrbroky">Twitch</a></button></div></div></div> 
+    <div class="card w-96 bg-base-100 shadow-xl"><figure class="px-10 pt-10"><img src="./gang.png" alt="Shoes" class="rounded-xl mask mask-hexagon"/></figure> 
+      <div class="card-body items-center text-center"><h2 class="card-title">Follow Me!</h2> 
+        <p>Posts, Clips, Reactions, Stories and Direct Message(DM).</p> 
+        <div class="card-actions"><button class="btn btn-primary"><a href="https://www.instagram.com/mrbroky">Instagram</a></button></div></div></div></div> 
+
+  <div class="grid grid-cols-4 mt-6 gap-4"></div> 
+
+  <div class="border mockup-window bg-base-300"><div class="flex justify-center px-4 py-16 bg-base-200"><iframe width="560" height="315" src="https://www.youtube.com/embed/PVJs596rp2o?start=335" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe></div></div> 
+  <footer class="items-center mt-6 p-4 footer bg-neutral text-neutral-content"><div class="items-center grid-flow-col"><p>Copyright \xA9 2022 - All right reserved</p></div>  
+    <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end"><a href="https://www.facebook.com/MrBroky"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16"><path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path></svg></a> 
+
+      <a href="https://www.twitter.com/Mr_Broky"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16"><path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"></path></svg></a> 
+
+      <a href="https://www.tiktok.com/@mrbroky?"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tiktok" viewBox="0 0 16 16"><path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3V0Z"></path></svg></a></div></footer>`,h(t,"class","svelte-1910pwy")},m(r,a){m(r,t,a)},p:i,i,o:i,d(r){r&&u(t)}}}class b extends l{constructor(t){super();n(this,t,null,v,d,{})}}new b({target:document.getElementById("app")});
